9ebf4dd77f4fc0c206ed95ad1e2754519ab4cf3b
AGISYA <abdiagisya@gmail.com>
2025-01-27 09:50:11 +0700

    pangxito lyout

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className="bg-red-50">
+    <div className="bg-white">
       <Navbar />
       <div className="relative flex items-center justify-center h-screen">
         <div className="relative w-full h-full">
